bda878d7a855b1ec7b9d1cc64d19bca4a7cbe467
Zakaria Wahyu <zakarianur6@gmail.com>
2023-12-10 22:18:59 +0700

    A Guessing Game

---
+++ b_guessing_game/src/main.rs
@@ -0,0 +1,37 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guset the number!");
+    println!("Please input your guess.");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number {}", secret_number);
+
+    loop {
+        // create mutable variable
+        let mut guess =  String::new();
+
+        // catch the user input
+        io::stdin()
+            .read_line(&mut guess) // return enum
+            .expect("Failed to read line");
+
+        println!("Your guessed {}", guess);
+
+        let guess_str:u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess_str.cmp(&secret_number){ // arm
+            Ordering::Less => println!("To small !!"),
+            Ordering::Greater => println!("To big !!"),
+            Ordering::Equal => {
+                println!("You winn !!");
+                break
+            }
+        }
+    }
+}
